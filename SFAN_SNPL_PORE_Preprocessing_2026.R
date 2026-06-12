34413e39b25fd6ce689aaf4687ef2d4521b260f4
DanielWoods <dwoods@nps.gov>
2026-06-12 13:42:02 -0600

    PK Changes to Daniel Branch (#2)
    * Updating Repo with 2025 data for 05/2026 data package
    
    * Updates to DRR script and processing of DRR for 2025 field season.
    
    * Removing redundant documentation- now located on sharepoint site
    
    * Updating tables and metadata with 5/19 Parker Kaye comments and new database FE and BE
    
    * 5/20/26 edits amending QC flags, column names in Bands and ChickBands
    
    * Updating DRR template and output to add dynamic QC Flag Template
    
    * Fixing error noted on 05/26/26 with QC flags
    
    * Adding new flag for invalid spatial coordinates according to PK feedback
    
    ---------
    
    Co-authored-by: KSherrill <kirk_sherrill@nps.gov>

--- SFAN_SNPL_PORE_Preprocessing_2026.R
+++ SFAN_SNPL_PORE_Preprocessing_2026.R
@@ -24,7 +24,11 @@ rm(list = ls())
 # Variables to Define
 #####################
 # SNPL PORE Backend Database with the Datasets to be preprocssed
+<<<<<<< HEAD
+db_name <- "//INPPORE07/Resources/Natural/SNPLOVER/SNPL_IM/DATA/Database/Dbase_BE/PORE_SNPL_BE_20260605.accdb"
+=======
 db_name <- "//INPPORE07/Resources/Natural/SNPLOVER/SNPL_IM/DATA/Database/Dbase_BE/PORE_SNPL_BE_20260514v2.accdb"
+>>>>>>> origin/main
 # Directory where output preprocessed .csv dataset files will be exported
 outPutDir <-'C:/Users/dwoods/GitHub/SFAN/SNPL_PORE_DataPackage/Data/Input'
 #############################################
